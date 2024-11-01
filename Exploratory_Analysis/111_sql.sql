318f9a95aac6d16c37cf2fd7e12ba243e490b28f
DomRowney <dominic.rowney@nhs.net>
2024-11-01 14:01:48 +0000

    Analysis - changed query to base data in query file, removed 999 after discussion with stakeholder

--- Exploratory_Analysis/111_sql.sql
+++ Exploratory_Analysis/111_sql.sql
@@ -1,62 +1,85 @@
-DECLARE 
- @StartDate AS DATE
-,@mainend AS DATE
-,@dailystart AS DATE
-,@dailyend AS DATE
-,@WeeklyEND AS DATE
-,@Latest_Month_end AS DATE
-,@Latest_Month_start AS DATE;
+-- ############
+-- Setup
+-- ############
 
-Set @StartDate = '2023-01-01'
-Set @MainEND   = (select max(a.[Start Date]) from [LocalDataNECS].[Ambulance].[vRX6_111] AS a LEFT JOIN [Reference].[dbo].[ref_Dates] AS b on CAST(a.[Start Date] AS DATE) = b.Full_Date where b.Weekday_Short_Name ='sun')
+-- removes messages
+SET NOCOUNT ON
 
--- the below needs amending AS soon AS the 111 weekly flow restarts 
+DECLARE @StartDate AS DATE
 
---SET @Latest_Month_end = (Select MAX(b.Month_End) from [TB4UW_Reporting].[NEAS111].[CallData] AS a LEFT JOIN [Reference].[dbo].[ref_Dates] AS b on CAST(a.[CALL CONNECT DATETIME] AS DATE) = b.Full_Date)-- automated do not amend unless needed. This bit finds the latest month start
---SET @Latest_Month_start = (Select MAX(b.Month_start) from [TB4UW_Reporting].[NEAS111].[CallData] AS a LEFT JOIN [Reference].[dbo].[ref_Dates] AS b on CAST(a.[CALL CONNECT DATETIME] AS DATE) = b.Full_Date)--
-
-SET @Latest_Month_end = (Select MAX(b.Month_End) from [LocalDataNECS].[Ambulance].[vRX6_111] AS a LEFT JOIN [Reference].[dbo].[ref_Dates] AS b on CAST(a.[Start Date] AS DATE) = b.Full_Date)-- automated do not amend unless needed. This bit finds the latest month start
-SET @Latest_Month_start = (Select MAX(b.Month_start) from [LocalDataNECS].[Ambulance].[vRX6_111] AS a LEFT JOIN [Reference].[dbo].[ref_Dates] AS b on CAST(a.[Start Date] AS DATE) = b.Full_Date)--
+SET @StartDate = 'REPLACE START DATE'
 
 DROP TABLE IF EXISTS #111_calls
 
+------------------------------------------------------------------
+
+-- ############
+-- 111 data
+-- ############
+
 SELECT 
-TOP 100
-	'111' AS [Indicator]
+--TOP 100
+	'111' AS [Start_Location]
 ,	r.[Call ID]
-,	r.[Pseudo NHS NUMBER]
+,	r.[Pseudo NHS NUMBER] AS [Pseudo NHS Number]
 ,	CAST(r.[Start Date] AS DATE) AS [CallDate]
 ,	CAST(r.[Start Date] AS DATETIME) AS [Call Connect Time]
 ,	d.[Weekday_Name]
 ,	CAST(d.[Week_Start] AS DATE) AS [Week_Start]
 ,	d.[Fin_Year] AS [Financial Year]
+
 ,	CASE 
-		WHEN r.[Report Post Code CCG Text] in ('NHS Newcastle Gateshead CCG','NHS Northumberland CCG','NHS North Tyneside CCG') THEN  'North ICP'
-		WHEN r.[Report Post Code CCG Text] in ('NHS Sunderland CCG','NHS South Tyneside CCG','NHS County Durham CCG','NHS Durham Dales Easington And Sedgefield CCG','NHS North Durham CCG') THEN  'Central ICP'
-		WHEN r.[Report Post Code CCG Text] in ('NHS Hartlepool And Stockton-on-tees CCG','NHS South Tees CCG','NHS Darlington CCG','NHS Tees Valley CCG') THEN  'South ICP'
-		ELSE 'Unknown'
-		END AS [ICP]
+		WHEN r.[Report Post Code CCG Text] IN ('NHS County Durham CCG'
+												,'NHS North Durham CCG'
+												,'NHS Durham Dales Easington And Sedgefield CCG'
+												,'Nhs Durham Dales, Easington And Sedgefield Ccg') THEN '84H'
+		WHEN r.[Report Post Code CCG Text] IN ('NHS Newcastle Gateshead CCG'
+												,'NHS Newcastle and Gateshead CCG'
+												,'Nhs Gateshead Ccg'
+												,'Nhs Newcastle West Ccg'
+												,'Nhs Newcastle North And East Ccg') THEN '13T'
+		WHEN r.[Report Post Code CCG Text] = 'Nhs North Tyneside Ccg' THEN '99C'
+		WHEN r.[Report Post Code CCG Text] = 'Nhs Northumberland Ccg' THEN '00L'
+		WHEN r.[Report Post Code CCG Text] = 'NHS South Tyneside CCG' THEN '00N'
+		WHEN r.[Report Post Code CCG Text] = 'NHS Sunderland CCG'	  THEN '00P'
+		WHEN r.[Report Post Code CCG Text] IN ('NHS Tees Valley CCG'
+												,'NHS South Tees CCG'
+												,'Nhs Hartlepool And Stockton-on-tees Ccg'
+												,'Nhs Darlington Ccg') THEN '16C'
+		WHEN  ISNULL(r.[Report Post Code CCG Text],'Unknown') = 'Unknown' THEN 'Unknown'
+		ELSE 'Out of Area'
+		END AS [Sub ICB Code]
+
 ,	CASE 
-		WHEN r.[Report Post Code CCG Text] In ('NHS County Durham CCG','NHS Durham Dales Easington And Sedgefield CCG','NHS North Durham CCG') THEN  'County Durham Sub ICB'
-		WHEN r.[Report Post Code CCG Text] In ('NHS Hartlepool And Stockton-on-tees CCG','NHS South Tees CCG','NHS Darlington CCG','NHS Tees Valley CCG') THEN  'Tees Valley Sub ICB'
-		ELSE replace(Replace(isnull([Report Post Code CCG Text],'Unknown'),'CCG','Sub ICB'),'NHS ','')
-		END AS [Sub ICB]
-,	ISNULL(f.[PCNDescription],'Unknown')AS [PCN]
-,	ISNULL(f.[PracticeDesc],'Unknown')	AS [GP Practice]
-,	ISNULL(f.[PracticeCode],'Unknown')	AS [Practice_Code]
+		WHEN r.[Report Post Code CCG Text] IN ('NHS County Durham CCG'
+												,'NHS North Durham CCG'
+												,'NHS Durham Dales Easington And Sedgefield CCG'
+												,'Nhs Durham Dales, Easington And Sedgefield Ccg') THEN 'County Durham'
+		WHEN r.[Report Post Code CCG Text] IN ('NHS Newcastle Gateshead CCG'
+												,'NHS Newcastle and Gateshead CCG'
+												,'Nhs Gateshead Ccg'
+												,'Nhs Newcastle West Ccg'
+												,'Nhs Newcastle North And East Ccg') THEN 'Newcastle Gateshead'
+		WHEN r.[Report Post Code CCG Text] = 'Nhs North Tyneside Ccg' THEN 'North Tyneside'
+		WHEN r.[Report Post Code CCG Text] = 'Nhs Northumberland Ccg' THEN 'Northumberland'
+		WHEN r.[Report Post Code CCG Text] = 'NHS South Tyneside CCG' THEN 'South Tyneside'
+		WHEN r.[Report Post Code CCG Text] = 'NHS Sunderland CCG'	  THEN 'Sunderland'
+		WHEN r.[Report Post Code CCG Text] IN ('NHS Tees Valley CCG'
+												,'NHS South Tees CCG'
+												,'Nhs Hartlepool And Stockton-on-tees Ccg'
+												,'Nhs Darlington Ccg') THEN 'Tees Valley'
+		WHEN  ISNULL(r.[Report Post Code CCG Text],'Unknown') = 'Unknown' THEN 'Unknown'
+		ELSE 'Out of Area'
+		END AS [Sub ICB Name]
+,	CAST('Unknown' AS varchar(max))	AS [GP Practice]
+,	ISNULL(LEFT(r.[GP National ID],6),'Unknown')	AS [GP Practice Code]
 ,	CASE
 		WHEN ISNULL(r.[Symptom_Group],'') = '' THEN  'Unknown'
-		WHEN r.[Symptom_Group] LIKE 'SG%' THEN  sg.[Symptom_Group_Description]
 		ELSE r.[Symptom_Group]
 		END AS [Symptom_Group]
-,	CASE
-		WHEN ISNULL(dp.[disposition],'') = '' THEN 'Unknown'
-		ELSE dp.[disposition]
-		END AS [Disposition_Group]
-,	CASE
-	WHEN ISNULL(dp.[UEC Lookup],'') = '' THEN 'Unknown'
-	ELSE dp.[UEC Lookup]
-	END AS [UEC_Lookup]
+,	r.[Final Disposition Code]
+,	CAST('Unknown' AS varchar(50))	AS [Disposition Group]
+,	CAST('Unknown' AS varchar(100)) AS [Disposition]
 ,	ISNULL(r.[In_Out_Hours],
 		CASE
 			WHEN d.[Weekday_Name] IN ('Saturday','Sunday') THEN  'Out of Hours'
@@ -64,17 +87,16 @@ TOP 100
 			WHEN DATEPART(HOUR, r.[Start Date]) BETWEEN 08 AND 17 THEN  'In Hours'
 			ELSE 'Out of Hours'
 			END) AS [In_Out_Hours]
-,	CASE WHEN [Last_Interaction_Type] = 'Non-PW Clinician'	THEN  1 ELSE 0 END AS [CAS Input]
-,	CASE WHEN [Call Connect Time] IS NOT NULL				THEN  1 ELSE 0 END AS [Contacts]
-,	CASE WHEN [Call Taker Triage Start] IS NOT NULL			THEN  1 ELSE 0 END AS [Call_Taker_Triages]
-,	CASE WHEN [Nurse Triage Start] IS NOT NULL				THEN  1 ELSE 0 END AS [Clinical_Triages]
-,	CASE WHEN (CAST(r.[Start Date] AS DATEtime) <= CASE WHEN @mainend = @Latest_Month_end THEN  CAST(r.[Start Date] AS DATEtime) else dateadd(dd,-1,@Latest_Month_start) end) THEN  1 else 0 end AS Latest_COMPLETE_FM_To_DATE
+,	CASE WHEN [Call Taker Triage Start] IS NOT NULL	THEN 'Yes' ELSE 'No' END AS [Call_Taker_Triages]
+,	CASE WHEN [Nurse Triage Start] IS NOT NULL		THEN 'Yes' ELSE 'No' END AS [Clinical_Triages]
 ,	r.dmicAge				AS [Patient Age]
 ,	ISNULL(r.[Patient Sex Text],'Unknown')	AS [Patient Sex]
 ,	CAST(NULL AS varchar)		AS [Outcome ID]
 ,	CAST(NULL AS datetime)		AS [Outcome Datetime]
 ,	CAST(NULL AS varchar(25))	AS [Outcome Type]
 ,	CAST(NULL AS varchar(50))	AS [Outcome]
+,	CAST(NULL AS varchar(50))	AS [Outcome Location Code]
+,	CAST(NULL AS varchar(100))	AS [Outcome Location Name]
 ,	CAST(NULL AS float)			AS [Hours to Outcome]
 INTO
 	#111_calls
@@ -82,52 +104,89 @@ FROM
 	[LocalDataNECS].[Ambulance].[vRX6_111] AS r
 	LEFT JOIN
 		[UK_Health_Dimensions].[dbo].[ref_Dates] AS d ON CAST(r.[Start Date] AS DATE) = d.[Full_Date]
-	LEFT JOIN (	SELECT DISTINCT
-						[Kc Look Up]
-					,	CASE 
-							WHEN [MDS Primary Split] like '%Recommended to Attend Other Service%' THEN  'Other Service'
-							WHEN [MDS Primary Split] like '%Not Recommended to Attend Other Service%' THEN  'No Recommendation'
-							WHEN [MDS Primary Split] like '%dispatch%' THEN  'Ambulance Dispatch'
-							WHEN [MDS Primary Split] like '%Primary and Community Care%' THEN  'Primary or Community Care'
-							WHEN [MDS Primary Split] like '%Recommended other outcome%'   THEN  'Recommended other outcome'
-							WHEN [MDS Primary Split] = 'Recommended to attend another service' THEN  'Recommended other outcome'
-							WHEN [MDS Primary Split] like '%Recommended to Attend A&E%' THEN  'Urgent or Emergency Care'
-							END AS Disposition,[UEC Lookup]
-					FROM [INFORMATIONTEAM].[dbo].[LU_DxCodes]
-				)	AS dp 
-				ON r.[Final Disposition Code] = dp.[Kc Look Up]		
-	LEFT JOIN (	SELECT DISTINCT
-				[PracticeCode],PracticeDesc,CCGDesc,CCGCode,LocalityDesc,legacy_CCG_Desc,PCNDescription
-				   FROM [PDC-SYS-SQL-142].[Raidr].[dbo].[LU_All_Practice_exclude_SurryHeatlands_1610_2023])
-				   AS f on 
-				   LEFT([PracticeCode],1) != 'v'
-				   AND LEFT([GP National ID],6) = f.[PracticeCode]
-				   AND (CASE 
-						WHEN r.[Report Post Code CCG Text] IN ('NHS County Durham CCG','NHS Durham Dales Easington And Sedgefield CCG','NHS North Durham CCG') THEN  'NHS County Durham CCG'
-						WHEN r.[Report Post Code CCG Text] IN ('NHS Hartlepool And Stockton-on-tees CCG','NHS South Tees CCG','NHS Darlington CCG','NHS Tees Valley CCG') THEN  'NHS Tees Valley CCG'
-						ELSE r.[Report Post Code CCG Text]
-						END) = f.[CCGDesc]
-	LEFT JOIN (	SELECT DISTINCT
-						[Symptom_Group_Code]
-					,	[Symptom_Group_Description]
-					FROM [UK_Health_Dimensions].[UEC_Analytics_Model].[Symptom_Groups_SCD]
-				) AS sg
-				ON r.[Symptom_Group] = SG.[Symptom_Group_Code]
+
 WHERE
 	1=1
+	AND r.[Pseudo NHS NUMBER] IS NOT NULL
+	AND r.[Pseudo NHS NUMBER] != ''
 	AND CAST(r.[Start Date] AS DATE) >= @StartDate
-	AND	[Report Post Code CCG Text] IN ('NHS Northumberland CCG'
-									,'NHS Newcastle Gateshead CCG'
-									,'NHS North Tyneside CCG'
-									,'NHS Hartlepool And Stockton-on-tees CCG'
-									,'NHS South Tees CCG'
-									,'NHS Darlington CCG'
-									,'NHS Tees Valley CCG'
-									,'NHS South Tyneside CCG'
-									,'NHS Sunderland CCG'
-									,'NHS County Durham CCG'
-									,'NHS Durham Dales Easington And Sedgefield CCG'
-									,'NHS North Durham CCG')
+	AND	[Report Post Code CCG Text] IN
+					('NHS County Durham CCG'
+					,'NHS North Durham CCG'
+					,'NHS Durham Dales Easington And Sedgefield CCG'
+					,'Nhs Durham Dales, Easington And Sedgefield Ccg'                  
+					,'NHS Newcastle Gateshead CCG'
+					,'NHS Newcastle and Gateshead CCG'
+					,'Nhs Gateshead Ccg'                  
+					,'Nhs Newcastle West Ccg'                  
+					,'Nhs Newcastle North And East Ccg'                  
+					,'Nhs North Tyneside Ccg'
+					,'Nhs Northumberland Ccg'                  
+					,'NHS South Tyneside CCG'
+					,'NHS Sunderland CCG'
+					,'NHS Tees Valley CCG'
+					,'NHS South Tees CCG'
+					,'Nhs Hartlepool And Stockton-on-tees Ccg'                  
+					,'Nhs Darlington Ccg');  
+
+------------------------------------------------------------------
+
+-- ############
+-- GP Practice
+-- ############
+
+UPDATE #111_calls
+SET
+	[GP Practice] = g.[Organisation_Name]
+FROM
+	[UK_Health_Dimensions].[ODS].[GP_Practices_And_Prescribing_CCs_SCD] AS g
+WHERE 
+	1=1
+	AND [GP Practice Code] = g.[Organisation_Code]
+	AND	g.[Is_Latest] = 1;
+
+------------------------------------------------------------------
+
+-- ############
+-- Disposition
+-- ############
+
+UPDATE #111_calls
+SET
+	[Disposition Group]	= CASE 
+							WHEN d.[MDS Primary Split] LIKE '%Recommended to Attend Other Service%' THEN  'Other Service'
+							WHEN d.[MDS Primary Split] LIKE '%Not Recommended to Attend Other Service%' THEN  'No Recommendation'
+							WHEN d.[MDS Primary Split] LIKE '%dispatch%' THEN  'Ambulance Dispatch'
+							WHEN d.[MDS Primary Split] LIKE '%Primary and Community Care%' THEN  'Primary or Community Care'
+							WHEN d.[MDS Primary Split] LIKE '%Recommended other outcome%'   THEN  'Recommended other outcome'
+							WHEN d.[MDS Primary Split] = 'Recommended to attend another service' THEN  'Recommended other outcome'
+							WHEN d.[MDS Primary Split] LIKE '%Recommended to Attend A&E%' THEN  'Urgent or Emergency Care'
+							END
+,	[Disposition]		= d.[UEC Lookup]
+FROM [INFORMATIONTEAM].[dbo].[LU_DxCodes] AS d
+WHERE
+	1=1
+	AND #111_calls.[Disposition Group] = 'Unknown'
+	AND #111_calls.[Final Disposition Code] = d.[Kc Look Up];
+
+------------------------------------------------------------------
+
+-- ############
+-- Symptom
+-- ############
+
+UPDATE #111_calls
+SET	[Symptom_Group] = s.[Symptom_Group_Description]
+FROM
+	[UK_Health_Dimensions].[UEC_Analytics_Model].[Symptom_Groups_SCD] AS s
+WHERE
+	1=1
+	AND s.[Is_Latest] = 1
+	AND #111_calls.[Symptom_Group] LIKE 'SG%'
+	AND #111_calls.[Symptom_Group] = s.[Symptom_Group_Code];
+
+	
+------------------------------------------------------------------
 
 -- ############
 -- A&E
@@ -142,11 +201,10 @@ SET
 							WHEN a.[attendance.location.department_type] IN ('03','04') THEN 'Urgent Treatment Centre'
 							END 
 ,	[Outcome]			= ISNULL(b.[ECDS_Group1],'Unknown')
+,	[Outcome Location Code] = a.[attendance.location.site]
 FROM
 	[LocalDataNECS].[ecds].[emergency_care]	AS a
-	LEFT JOIN	(	SELECT DISTINCT [PRIMARYKEY_ID]
-					FROM [LocalDataNECS].[ECDS].[commissioning.national_pricing.exclusion_reasons]
-					) AS x 
+	LEFT JOIN [LocalDataNECS].[ECDS].[commissioning.national_pricing.exclusion_reasons] AS x 
 				ON a.[PRIMARYKEY_ID] = x.[PRIMARYKEY_ID]
 	LEFT JOIN ( SELECT DISTINCT
 						[SNOMED_Code]
@@ -173,16 +231,18 @@ WHERE
 	AND x.[PRIMARYKEY_ID] IS NULL -- exclude streamed attendances - only looking at actual triaged attendances !! 
 	;
 
+------------------------------------------------------------------
+
 -- ############
 -- Urgent Care
 -- ############
 
 UPDATE #111_calls
 SET
-	[Outcome ID]		= a.[PRIMARYKEY_ID]
+	[Outcome ID]		= a.[Pseudo CaseID]
 ,	[Outcome Datetime]	= CAST(a.[CaseStarted] AS datetime)
 ,	[Outcome Type]		= 'Urgent Treatment Centre'
-,	[Outcome Type]		= CASE
+,	[Outcome]			= CASE
 							WHEN a.[FollowUps] LIKE '%Patient Deceased%' THEN 'Died'
 							WHEN a.[FollowUps] LIKE '%Admitted to Hospital%' THEN 'Admitted'			
 							-- might need more follow ups
@@ -190,15 +250,59 @@ SET
 							WHEN a.[num booked] = 1 THEN 'Urgent Treatment Centre'
 							ELSE 'Unknown'
 							END
+,	[Outcome Location Code] = a.[CaseClosedAt]
+,	[Outcome Location Name] = a.[CaseClosedAt]
 FROM
 	[LocalDataNECS].[Acute].[RXP_UrgentCareCentres] AS a
 WHERE
 	1=1
 	AND a.[Pseudo NHSNumber] = #111_calls.[Pseudo NHS NUMBER]
-	AND ISNULL(#111_calls.[Outcome Datetime],SYSDATETIME()) >  CAST(a.[attendance.arrival.date] AS datetime)+CAST(a.[attendance.arrival.time] AS datetime)
+	AND ISNULL(#111_calls.[Outcome Datetime],SYSDATETIME()) >  CAST(a.[CaseStarted] AS datetime)
 	AND DATEDIFF(SECOND
                 ,#111_calls.[Call Connect Time]
                 ,CAST(a.[CaseStarted] AS datetime))
 			BETWEEN 0 AND (90000-1) --seconds in 24 hours
-	AND CAST(a.[attendance.arrival.date] AS DATE) >= @StartDate
+	AND CAST(a.[CaseStarted] AS DATE) >= @StartDate
 	AND ISNULL(a.[num walkins],0) + ISNULL(a.[num booked],0) > 0;
+
+
+------------------------------------------------------------------
+
+-- ############
+-- Derived
+-- ############
+
+UPDATE #111_calls
+SET [Outcome Location Name] = t.[Organisation_Name]
+FROM
+	[UK_Health_Dimensions].[ODS].[NHS_Trusts_And_Trust_Sites_SCD] AS t
+WHERE
+	1=1
+	AND t.[Is_Latest] = 1
+	AND #111_calls.[Outcome Location Code] = t.[Organisation_Code];
+
+UPDATE #111_calls
+SET [Outcome Type] = 'No UEC Contact'
+,	[Outcome] = [Disposition]
+,	[Outcome Location Code] = 'No UEC Contact'
+,	[Outcome Location Name] = 'No UEC Contact'
+,	[Hours to Outcome] = 0.0
+WHERE [Outcome Type] IS NULL;
+
+
+UPDATE #111_calls
+SET [Hours to Outcome] = CAST(DATEDIFF(MINUTE
+                ,[Call Connect Time]
+                ,[Outcome Datetime])
+					AS float)/60.0
+WHERE [Outcome Datetime] IS NOT NULL;
+
+
+
+------------------------------------------------------------------
+
+-- ############
+-- Output
+-- ############
+
+SELECT * FROM #111_calls
